87ddb91b230274467c222e83c49ab40f916adba3
Guilherme Palma <guilhermepallma@gmail.com>
2022-08-01 00:15:19 -0400

    ajuste na configuração inicial do redux

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,10 +3,10 @@ const INITIAL_STATE = {
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
- switch(action.type) {
-   default:
+  switch (action.type) {
+  default:
     return state;
- }
-}
+  }
+};
 
 export default userReducer;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -2,14 +2,14 @@ const INITIAL_STATE = {
   currencies: [], // array de string
   expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
   editor: false, // valor booleano que indica de uma despesa está sendo editada
-  idToEdit: 0, // valor numérico que armazena o id da despesa que 
+  idToEdit: 0, // valor numérico que armazena o id da despesa que
 };
 
 const walletReducer = (state = INITIAL_STATE, action) => {
- switch(action.type) {
-   default:
+  switch (action.type) {
+  default:
     return state;
- }
-}
+  }
+};
 
 export default walletReducer;
